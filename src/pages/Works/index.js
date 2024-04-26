be51cf37dfd95ff09fa5c2459d501e98051f2c48
ResplendentGrave <1316792949@qq.com>
2024-04-26 20:55:52 +0900

    chore

--- src/pages/Works/index.js
+++ src/pages/Works/index.js
@@ -17,11 +17,11 @@ const Works = () => {
             </Title>
 
             <div className="row">
-                <WorkItem
+                {/* <WorkItem
                     images=""
                     title=""
-                />
-                {/* {
+                /> */}
+                {
                     selectedProjects.map(
                         item => <div className="column" key={item.sourceCodeLink}>
                             <WorkItem
@@ -29,8 +29,66 @@ const Works = () => {
                             />
                         </div>
                     )
-                } */}
+                }
             </div>
+            <div className='row'>
+                <WorkItem
+                {
+                    ... 
+                    {
+                        title: 'React portfolio',
+                        images: [
+                            "https://i.ibb.co/3TqgZRr/shot20.jpg" 
+                        ],
+                        sourceCodeLink: 'https://github.com/ResplendentGrave/myportfolio',
+                        demoLink: '',
+                        type: 'react'
+                    }
+                }
+                
+                />
+            </div>
+            <div className='row'>
+               
+                <WorkItem
+                {
+                    ... 
+                    {
+                        title: 'React flea market management site',
+                        images: [
+                            "https://i.ibb.co/dDRnqfb/shot30.jpg",
+                            "https://i.ibb.co/w4mxr7Y/shot31.jpg"
+                        ],
+                        sourceCodeLink: 'https://github.com/ResplendentGrave/react_manage_site',
+                        demoLink: '',
+                        type: 'react'
+                    }
+                }
+                
+                />
+                 <WorkItem
+                {
+                    ... 
+                    {
+                        title: 'Weixin flea market',
+                        images: [
+                            "https://i.ibb.co/R6WFtb4/shot34.jpg",
+                            "https://i.ibb.co/PNkp7bj/shot33.jpg",
+                            "https://i.ibb.co/x8stHR4/shot32.jpg"
+                        ],
+                        sourceCodeLink: 'https://github.com/ResplendentGrave/wx_fleamarket',
+                        demoLink: '',
+                        type: 'react'
+                    }
+                }
+                
+                />
+            </div>
+            <Title>        
+                ARTICLES
+            </Title>
+           
+
         </div>
     );
 };
